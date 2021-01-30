74f7c4d117a098a37165acfbcdffbaa0585fd940
omangbaheti <60500898+omangbaheti@users.noreply.github.com>
2021-01-30 21:41:53 +0530

    Tatti

--- Assets/Karting/Scripts/GameModes/PickupObjectWithPizazz.cs
+++ Assets/Karting/Scripts/GameModes/PickupObjectWithPizazz.cs
@@ -3,7 +3,7 @@
 /// <summary>
 /// This class inherits from TargetObject and represents a PickupObject.
 /// </summary>
-public class PickupObjectWithPizazz : TargetObject
+public class PickupObject : TargetObject
 {
     [Header("PickupObject")]
 
@@ -16,13 +16,11 @@ public class PickupObjectWithPizazz : TargetObject
     [Tooltip("Destroy this gameobject after collectDuration seconds")]
     public float collectDuration = 0f;
 
-    private GameObject Ring;
-    public  Material onPickup;
+ 
 
 
     void Start() 
     {
-        Ring = transform.GetChild(0).gameObject;
         Register();
     }
 
@@ -35,16 +33,16 @@ public class PickupObjectWithPizazz : TargetObject
 
         if (spawnPrefabOnPickup)
         {
+            Debug.Log("Confetti");
             var vfx = Instantiate(spawnPrefabOnPickup, CollectVFXSpawnPoint.position, Quaternion.identity);
-            Debug.Log(vfx.GetComponent<ParticleSystem>().isPlaying);
-            //Destroy(vfx, destroySpawnPrefabDelay);
+            Destroy(vfx, destroySpawnPrefabDelay);
         }
                
         Objective.OnUnregisterPickup(this);
+
         TimeManager.OnAdjustTime(TimeGained);
-        Debug.Log(Ring.GetComponent<Renderer>().material);
-        Ring.GetComponent<Renderer>().material = onPickup;
-        Debug.Log(Ring.GetComponent<Renderer>().material);
+
+
         Destroy(gameObject, collectDuration);
     }
     
